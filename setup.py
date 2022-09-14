6b214988f24f7da9f387acc4812141825e769288
Jeremy Brown <jeremyjjbrown@gmail.com>
2022-09-14 11:43:33 -0500

    make an installable project

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import sys
+
+if sys.version_info < (3, 10):
+    sys.exit('Sorry, Python < 3.10 is not supported')
+
+setup(
+    name='simple-dvr',
+    version='0.0.1',
+    packages=find_packages(),
+    python_requires='>=3.10',
+    include_package_data=True,
+    license='Properitary unless otherwise stated',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'APScheduler>=3.9.1',
+        'Flask>=2.2.2',
+        'Jinja2>=2.11.2',
+        'click>=8.1.3',
+        'eventlet>=0.33.1',
+        'flake8==3.8.4',
+        'pytest>=6.2.1',
+        'requests>=2.25.1'
+    ],
+    entry_points={
+    'console_scripts': [
+        'recorder = simple_dvr.recorder:cli',
+    ],
+},
+)
